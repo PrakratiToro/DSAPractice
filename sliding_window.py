c1bc99087f6877903c4a4ce01e7d4170a2b9fbeb
Prakrati Jain <Prakriti.Jain@toro.com>
2026-06-25 12:46:40 +0530

    Update project

--- sliding_window.py
+++ sliding_window.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 def sliding_window_fixed(l,k):
     window_sum = 0
     for i in range(k):
@@ -11,3 +13,17 @@ def sliding_window_fixed(l,k):
         largest = max(largest,window_sum)
     return largest
 print(sliding_window_fixed([1,2,3,7,4,1],3))
+
+def sliding_window_dynamic(s):
+    longest = 0
+    l = 0
+    counter : dict[str,int] = defaultdict(int)
+    for r in range(len(s)):
+        counter[s[r]] +=1
+        while counter[s[r]] > 1:
+            counter[s[l]] -=1
+            l+=1
+        longest = max(longest,r-l+1)
+    return (longest)
+   
+print(sliding_window_dynamic("abcdbea"))
